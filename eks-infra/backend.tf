6581196c0dfd7ed1072f1096e1d696f5d6c78591
satyajit <satyajit.1418@gmail.com>
2026-08-27 22:35:38 +0530

    update backend.tf

--- eks-infra/backend.tf
+++ eks-infra/backend.tf
@@ -1,8 +1,8 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "astro-state-bucket"
-#     key = "terraform.tfstate"
-#     region = "us-east-1"
-#     dynamodb_table = "astro-state-lock"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "astro-state-bucket"
+    key = "terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "astro-state-lock"
+  }
+}
