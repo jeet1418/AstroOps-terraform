4fb7ab3adc609f8289e96567c0aa9c479c23fe4b
satyajit <satyajit.1418@gmail.com>
2026-08-27 22:16:39 +0530

    update eks-infra/modules/eks/main.tf

--- eks-infra/modules/eks/main.tf
+++ eks-infra/modules/eks/main.tf
@@ -26,6 +26,10 @@ resource "aws_eks_cluster" "main" {
   role_arn = aws_iam_role.cluster.arn
   version  = var.kubernetes_version
 
+  access_config {
+    authentication_mode = "API_AND_CONFIG_MAP"
+  }
+
   vpc_config {
     subnet_ids = var.subnet_ids
     endpoint_public_access = var.enable_public_access
